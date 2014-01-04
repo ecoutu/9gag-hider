a5ad1a33c89058f35d1788ae0b93c8ad6a077b3d
Eric Coutu <eric.coutu@gmail.com>
2014-01-04 11:24:06 -0500

    displaying 'unknown title' for hidden posts that weren't able to fetch the title due to ajax loading

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   for (var key in hiddenList) {
     var hiddenItem = hiddenList[key];
     var title = hiddenItem.title.trim() || "Unknown title";
-    $("ul#hiddenList").append('<li>Hid '+hiddenItem.type+' post <a href="'+hiddenItem.url+'">'+hiddenItem.title+'</a></li>');
+    console.log(title);
+    $("ul#hiddenList").append('<li>Hid '+hiddenItem.type+' post <a href="'+hiddenItem.url+'">'+title+'</a></li>');
   }
 });
